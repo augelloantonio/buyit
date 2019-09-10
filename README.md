4315d172e4ef2893e665ab5fb956494e2e6778ad
gello94 <gello94@live.it>
2019-09-10 15:37:59 +0100

    template imported and account app created

---
+++ README.md
@@ -0,0 +1,1 @@
+Template taken from https://startbootstrap.com/templates/shop-homepage/
---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import urls_reset
+from .views import index, register, profile, logout, login
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('logout/', logout, name='logout'),
+    path('profile/', profile, name='profile'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('reset-password/', include(urls_reset))
+]
