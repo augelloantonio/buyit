1d7695b72e6455ee05c390f84f699ef39600708b
gello94 <gello94@live.it>
2019-09-10 19:45:47 +0100

    static direction and media direction added in setting

--- buyit/settings.py
+++ buyit/settings.py
@@ -127,4 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
