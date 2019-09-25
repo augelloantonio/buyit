4105e5f36294faf1e4f98216782fcefda8b975fa
gello94 <gello94@live.it>
2019-09-25 14:29:15 +0100

    added url for media images in pattern

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import all_products
 
 urlpatterns = [
     path('', all_products, name='products'),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
