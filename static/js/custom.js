ad3f7f3fc687c4e48184296d9542a217b8692266
gello94 <gello94@live.it>
2019-09-27 22:47:22 +0100

    changed name in custom js

---
+++ static/js/custom.js
@@ -0,0 +1,17 @@
+/**
+ * This function will give to the navbar the effect to hide it scrolling down by
+ * moving the navbar out the viewport of the user adding the style "top: -50px";
+ * When the page scroll up the bar will appear again by adding back the style "top: 0".
+ * The scrolling has a transition effect added in the css file
+ */
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
