39cf78d0f2f9b5e88a9a8e619db35df46f9b76c8
gello94 <gello94@live.it>
2019-09-28 15:38:34 +0100

    removed import all_products

--- home/views.py
+++ home/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Product
-from products.views import all_products
-
 
 # Create your views here.
 def index(request):
     products = Product.objects.all()
     return render(request, "index.html", {"products": products})
-
-
-
