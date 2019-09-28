6df6fb80d0d3e48c94c02dd9d0c5707c10374081
gello94 <gello94@live.it>
2019-09-28 19:16:31 +0100

    order app added with the possibilities to make an order and for the admin of the website to see a list of orders made

--- orders/models.py
+++ orders/models.py
@@ -6,6 +6,7 @@ from products.models import Product
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
+    email_address = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
@@ -18,8 +19,8 @@ class Order(models.Model):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT, null=False)
-    product = models.ForeignKey(Product,on_delete=models.PROTECT, null=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, null=False)
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import checkout
+from . import views
+from .views import checkout, all_orders, order_detail
 
 urlpatterns = [
-    path('', checkout, name='checkout')
+    path('', checkout, name='checkout'),
+    path('orders', all_orders, name='all_orders'),
+    path('order_detail/<int:id>', views.order_detail, name="order_detail"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
