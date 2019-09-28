7c639939a5da51f640fa0a5ec019b8a5fa1afa45
gello94 <gello94@live.it>
2019-09-28 16:19:06 +0100

    added delete product def

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 from .forms import ProductForm
+from home.views import index
 
 # Create your views here.
 
@@ -26,18 +27,39 @@ def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, "productdetail.html", {'product': product})
 
-    def create_or_edit_product(request, pk=None):
-        """
-    Create a view that allows us to create
-    or edit a post depending if the Post ID
-    is null or not
-    """
-    product = get_object_or_404(Product, pk=pk) if pk else None
+
+def add_a_product(request):
     if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES, instance=product)
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            product = form.save()
-            return redirect(product_detail, product.pk)
+            form.save()
+            return redirect(index)
     else:
-        form = ProductForm(instance=product)
-    return render(request, 'productform.html', {'form': form})
+        form = ProductForm()
+
+    return render(request, "productform.html", {'form': form})
+
+
+def edit_product(request, id):
+    item = get_object_or_404(Item, pk=id)
+
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect(product_detail)
+    else:
+        form = ItemForm(instance=item)
+    return render(request, "item_form.html", {'form': form})
+
+def remove_product(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    item.delete()
+    return redirect(index)
+
+
+def toggle_status(request, id):
+    item = get_object_or_404(Product, pk=id)
+    item.done = not item.done
+    item.save()
+    return redirect(get_todo_list)
