128178bf59170325c0d4eab93cbdcd59041a4f64
gello94 <gello94@live.it>
2019-09-28 14:50:09 +0100

    added context processor to let cart context  work

--- buyit/settings.py
+++ buyit/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'home',
     'products',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
+
             ],
         },
     },
