4ef98ea287fb0faf31e0da15929a5746897c0671
gello94 <gello94@live.it>
2019-09-29 21:29:03 +0100

    added url to order details

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import dashboard, dashboard_orders
+from .views import dashboard, dashboard_orders, dashboard_order_details
 
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('dashboardorders', views.dashboard_orders, name='dashboard_orders'),
+    path('dashboardorder/<int:id>', views.dashboard_order_details, name='dashboard_order_details'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
