d9ee07e7f82372f3ed7fcf85fba27a3b8b643ac9
gello94 <gello94@live.it>
2019-09-29 21:30:06 +0100

    added viewing class for order details and fix to show all order info added in class

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from orders.models import OrderLineItem, Order
+from products.models import Product
 
 
 @login_required
@@ -8,8 +9,19 @@ def dashboard(request):
     orders = Order.objects.all()
     return render(request, "dashboard.html", {"orders": orders})
 
+
 @login_required
 def dashboard_orders(request):
     orders = Order.objects.all()
-    return render(request, "dashboardorders.html", {"orders": orders})
+    order_info = OrderLineItem.objects.all()
+    product = Product.objects.all()
+    
+    return render(request, "dashboardorders.html", {"order_info": order_info, "orders": orders, 'product':product})
 
+@login_required
+def dashboard_order_details(request, id):
+    orders = Order.objects.all()
+    order_info = OrderLineItem.objects.all()
+    order = get_object_or_404(Order, id=id)
+    
+    return render(request, "dashboardordersdetails.html", {"order_info": order_info, "orders": orders, 'order':order})
