3b7d27cf6bcd2aac879561594ce5598e9a52f109
gello94 <gello94@live.it>
2019-09-30 12:41:35 +0100

    added url for product dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls.static import static
 from . import views
 from .views import dashboard, dashboard_orders, dashboard_order_details
 
-
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('dashboardorders', views.dashboard_orders, name='dashboard_orders'),
     path('dashboardorder/<int:id>', views.dashboard_order_details, name='dashboard_order_details'),
+    path('dashboardproducts', views.dashboard_product, name='dashboard_product'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
