d5956c946cb7832e72ac6d20f06cf07e4e72e9a1
gello94 <gello94@live.it>
2019-09-30 17:01:04 +0100

    added filtered search

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Product
 
-# Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ('name', 'category', 'in_stock')
+    list_filter = ['name', 'category', 'published_date', 'in_stock']
+    search_fields = ['name', 'category']
+    
+
+admin.site.register(Product, ProductAdmin)
