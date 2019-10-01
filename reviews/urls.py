b5ff30a4e69c90f5699be83c7f7ea9d16e733e65
gello94 <gello94@live.it>
2019-10-01 14:03:51 +0100

    added urls for review

---
+++ reviews/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from .views import add_review
+
+urlpatterns = [
+    path('addreview/<int:product_id>', views.add_review, name='add_review'),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
