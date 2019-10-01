27de3c26482de3b6c73ba50ce2ebe0000433671b
gello94 <gello94@live.it>
2019-10-01 16:50:29 +0100

    added url to render reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import add_review
+from .views import add_review, review_detail
 
 urlpatterns = [
-    path('addreview/<int:product_id>', views.add_review, name='add_review'),
+    path('review/<int:review_id>', views.review_detail, name='review_detail'),
+    path('addreview/product/<int:product_id>', views.add_review, name='add_review'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
