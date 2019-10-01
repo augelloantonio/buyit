d0c38bca20ced92c89c4cd8785af5af03b4c584a
gello94 <gello94@live.it>
2019-10-01 16:50:46 +0100

    added filtered product in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,7 +4,7 @@ from .models import Review
 class ReviewAdmin(admin.ModelAdmin):
     model = Review
     list_display = ('product', 'rating', 'user_name', 'comment', 'pub_date')
-    list_filter = ['pub_date', 'user_name']
+    list_filter = ['pub_date', 'user_name', 'product', 'rating']
     search_fields = ['comment']
     
 admin.site.register(Review, ReviewAdmin)
