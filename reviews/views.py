6653137108bb2423edd6e063e785cceb42257d40
gello94 <gello94@live.it>
2019-10-01 16:50:11 +0100

    fixed add review form and added a viewwing list of review for every product

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from products.models import Product
+from .models import Review
+import datetime
+
+
+def review_list(request):
+    reviews = Review.objects.all()
+    product = Product.objects.all()
+    for review in reviews:
+        if review.product_id == product.id:
+            review_list = Review.objects.all().order_by('-pub_date')[:9]
+    return ({"review_list": review_list})
+
+
+def review_detail(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    return render(request, 'reviews/review_detail.html', {'review': review})
+
 
 def add_review(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
@@ -10,16 +28,12 @@ def add_review(request, product_id):
         comment = form.cleaned_data['comment']
         user_name = request.user
         review = Review()
-        review.wine = wine
+        review.product_id = product.id
         review.user_name = user_name
         review.rating = rating
         review.comment = comment
         review.pub_date = datetime.datetime.now()
         review.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
+        return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
 
-    return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
-        
+    return render(request, "addreview.html", {'form': form, 'product': product})
