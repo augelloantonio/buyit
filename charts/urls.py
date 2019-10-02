01aa2f8b8844be129ff49a62c9adc4633b3d09bf
gello94 <gello94@live.it>
2019-10-02 17:57:10 +0100

    added url to get data from json

--- charts/urls.py
+++ charts/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import view_chart, get_data 
+from .views import view_chart, get_data, ChartData
 
 urlpatterns = [
     path('', views.view_chart, name='view_chart'),
     path('data/', views.get_data, name='get_data'),
+    path('chart/data', ChartData.as_view()),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
