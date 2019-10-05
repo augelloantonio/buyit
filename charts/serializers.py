f5c60b0cb61a1e87ce342f9dd0d750308642aa3c
gello94 <gello94@live.it>
2019-10-05 21:24:47 +0100

    added serializer to serialize data from db to json

---
+++ charts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class ProductSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=120)
+    price = serializers.CharField()
+    category = serializers.CharField()
+
+class MonthlyEarning(serializers.Serializer):
+    total = serializers.CharField()
+    date = serializers.DateField()
