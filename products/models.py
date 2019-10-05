32fce92438ef384da68a557fa5a0ade0b71a8044
gello94 <gello94@live.it>
2019-10-05 21:26:05 +0100

    added model product sold to count total product sold

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
         blank=True, null=True, default=timezone.now)
     category = models.CharField(max_length=100, choices=categories_list, null=True, blank=True)
     in_stock = models.BooleanField(blank=False, default=True)
+    quantity_sold = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
