96523bfc193e034b4f7f8fd2bc915f70f2bdb0d9
gello94 <gello94@live.it>
2019-10-05 21:33:54 +0100

    added urls for charting

--- charts/urls.py
+++ charts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import view_chart, get_data, ChartData
+from .views import view_chart, ChartData, view_chart_orders
 
 urlpatterns = [
     path('', views.view_chart, name='view_chart'),
-    path('data/', views.get_data, name='get_data'),
+    path('', views.view_chart_orders, name='view_chart_orders'),
     path('chart/data', ChartData.as_view()),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
