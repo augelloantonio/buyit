c5e8b5c997d42a2cb6dc7ee7c4a0a1ab5faacf8d
gello94 <gello94@live.it>
2019-10-06 19:35:22 +0100

    optimized the system to count the object in the dictionary

--- dashboard/views.py
+++ dashboard/views.py
@@ -12,33 +12,20 @@ from django.db.models.functions import TruncMonth, TruncYear
 def dashboard(request):
     orders = Order.objects.all()
     order_info = OrderLineItem.objects.all()
-    products = Product.objects.all()
+    product = Product.objects.all()
     reviews = Review.objects.all()
 
     total_orders_earning = sum(items.total for items in order_info)
     monthly_orders_earning = (total_orders_earning / 12)
-    total_orders = 0
+    total_orders = orders.count()
     total_product_sold = 0
-    total_products = 0
-    total_products_in_stock = 0
-    total_products_not_stock = 0
-    total_reviews = 0
-
-    for items in order_info:
-        total_orders += 1
+    total_products = product.count()
+    total_products_in_stock = product.exclude(in_stock=False).count()
+    total_products_not_stock = product.exclude(in_stock=True).count()
+    total_reviews = reviews.count()
 
     for items in order_info:
         total_product_sold = sum(items.quantity for items in order_info)
-
-    for product in products:
-        total_products += 1
-        if product.in_stock == True:
-            total_products_in_stock += 1
-        else:
-            total_products_not_stock += 1
-
-    for review in reviews:
-        total_reviews += 1
 
     order_by_date = order_info.filter(order__date__year='2019').values_list(
         'order__date__month').annotate(total_order_price=Count('total'))
