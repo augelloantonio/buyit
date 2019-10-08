6e1913c4c12748a5c619aed13a81926997e4fbb6
gello94 <gello94@live.it>
2019-10-08 16:47:57 +0100

    added new total price with discount code

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from voucher.models import Voucher
+from voucher.forms import VoucherForm
 
 
 def cart_contents(request):
@@ -8,15 +10,23 @@ def cart_contents(request):
     every page
     """
     cart = request.session.get('cart', {})
+    voucher_id = request.session.get('voucher_id')
 
     cart_items = []
     total = 0
     product_count = 0
-    
+    new_total = 0
+
+    code = Voucher.objects.get(id=voucher_id)
+   
+
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
+        new_total = total - code.price_reducing
         product_count += quantity
         cart_items.append({'id': id, 'quantity': quantity, 'product': product})
-    
-    return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
+
+    voucher_form = VoucherForm(request.POST)
+    return {'cart_items': cart_items, 'total': total, 'product_count': product_count, 'voucher_form':voucher_form,
+    'new_total':new_total}
