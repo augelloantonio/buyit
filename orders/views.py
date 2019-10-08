67b95567a66d56a71e48fb452010b76a559b6952
gello94 <gello94@live.it>
2019-10-08 21:23:27 +0100

    enabled stripe payment and implemented voucher full working

--- orders/views.py
+++ orders/views.py
@@ -8,38 +8,54 @@ from django.conf import settings
 from django.utils import timezone
 from products.models import Product
 from voucher.models import Voucher
+from decimal import Decimal
 import stripe
 
 # Create your views here.
-""" stripe.api_key = settings.STRIPE_SECRET"""
+stripe.api_key = settings.STRIPE_SECRET
 
 
 @login_required()
 def checkout(request):
     if request.method == "POST":
-        #apply voucher code
-        
+
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
-    
+        # Handle bug if voucher_id is not in session
+        if request.session['voucher_id'] != None:
+            voucher_id = request.session.get('voucher_id')
+            code = Voucher.objects.get(id=voucher_id)
+        else:
+            None
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
+
+            order.save()
+
             cart = request.session.get('cart', {})
             total = 0
+            new_total = 0
             for id, quantity in cart.items():
                 product = get_object_or_404(Product, pk=id)
-                total = quantity * product.price                    
+                total += quantity * product.price
+                if request.session['voucher_id'] != None:
+                    discount = (code.price_reducing/Decimal('100'))*total
+                    new_total = total - discount
+                else:
+                    new_total = total
                 order_line_item = OrderLineItem(
                     order=order,
                     product=product,
                     quantity=quantity,
-                    total=total,
+                    total=new_total,
                 )
-                
+                order_line_item.save()
+                product.quantity_sold == quantity
+                product.save()
             try:
                 customer = stripe.Charge.create(
-                    amount=int(total * 100),
+                    amount=int(new_total * 100),
                     currency="EUR",
                     description=request.user.email,
                     card=payment_form.cleaned_data['stripe_id'],
@@ -49,23 +65,21 @@ def checkout(request):
 
             if customer.paid:
                 messages.error(request, "You have successfully paid")
+                # empty session cart
                 request.session['cart'] = {}
-                return redirect(reverse('products'))
-                product.quantity_sold == quantity
-                product.save()
-                order_line_item.save()
-                order.save()
+                # Remove voucher from session
+                request.session['voucher_id'] = None
+                return redirect(reverse('index'))
             else:
                 messages.error(request, "Unable to take payment")
         else:
             print(payment_form.errors)
-            messages.error(
-                request, "We were unable to take a payment with that card!")
+            messages.error(request, "We were unable to take a payment with that card!")
     else:
         payment_form = MakePaymentForm()
         order_form = OrderForm()
 
-    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form})
+    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
 
 
 def all_orders(request):
