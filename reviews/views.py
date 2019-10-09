b08587d061afd9e6d2e95b9a95a8e0b6572a1e71
gello94 <gello94@live.it>
2019-10-09 12:29:42 +0100

    user not allowed to leave a review twice on a product

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.contrib import messages
+import datetime
 from .forms import ReviewForm
 from products.models import Product
 from .models import Review
-import datetime
+
 
 
 def review_detail(request, review_id):
@@ -15,17 +17,23 @@ def review_detail(request, review_id):
 def add_review(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     form = ReviewForm(request.POST)
-    if form.is_valid():
-        rating = form.cleaned_data['rating']
-        comment = form.cleaned_data['comment']
-        user_name = request.user
-        review = Review()
-        review.product_id = product.id
-        review.user_name = user_name
-        review.rating = rating
-        review.comment = comment
-        review.pub_date = datetime.datetime.now()
-        review.save()
-        return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
+    user = request.user
 
-    return render(request, "addreview.html", {'form': form, 'product': product})
+    if Review.objects.filter(product_id=product, user__username=user):
+        messages.error(request, "We are sorry, but you can't review twice!")
+        return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
+    else:
+        if form.is_valid():
+            rating = form.cleaned_data['rating']
+            comment = form.cleaned_data['comment']
+            user_name = request.user
+            review = Review()
+            review.product_id = product.id
+            review.user_name = user_name
+            review.rating = rating
+            review.comment = comment
+            review.pub_date = datetime.datetime.now()
+            review.save()
+            return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
+
+        return render(request, "addreview.html", {'form': form, 'product': product})
