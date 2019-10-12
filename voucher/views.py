0b176502dcabd23c27ca68e0bed4a55860408aa0
gello94 <gello94@live.it>
2019-10-12 16:54:10 +0100

    giving to the voucer a result of none to remove code from cart if user use a different code

--- voucher/views.py
+++ voucher/views.py
@@ -13,15 +13,18 @@ from django.views.decorators.http import require_POST
 
 @require_POST
 def get_voucher(request):
-    voucher_form = VoucherForm(request.POST)
-    now = timezone.now()
-    if voucher_form.is_valid():
-        code = voucher_form.cleaned_data['code']
-        try:
-            voucher = Voucher.objects.get(code=code,
-                                          active=True)
-            request.session['voucher_id'] = voucher.id
-            messages.success(request, 'Coupon Accepted.')
-        except ObjectDoesNotExist:
-            messages.success(request, 'Coupon Does not exist.')
-    return redirect("view_cart")
+    if request.method == 'POST':
+        voucher_form = VoucherForm(request.POST)
+        now = timezone.now()
+        if voucher_form.is_valid():
+            code = voucher_form.cleaned_data['code']
+            try:
+                voucher = Voucher.objects.get(code=code,
+                                            active=True)
+                request.session['voucher_id'] = voucher.id
+                messages.success(request, 'Coupon Accepted.')
+            except ObjectDoesNotExist:
+                messages.success(request, 'Coupon Does not exist.')
+                request.session['voucher_id'] = None
+                code = None
+        return redirect("view_cart")
