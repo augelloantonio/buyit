1ee71177c2ba6bd331a755e095b0cc1cb0218dc0
gello94 <gello94@live.it>
2019-10-14 21:19:03 +0100

    resolved bug of session

--- voucher/views.py
+++ voucher/views.py
@@ -15,16 +15,14 @@ from django.views.decorators.http import require_POST
 def get_voucher(request):
     if request.method == 'POST':
         voucher_form = VoucherForm(request.POST)
-        now = timezone.now()
         if voucher_form.is_valid():
             code = voucher_form.cleaned_data['code']
             try:
                 voucher = Voucher.objects.get(code=code,
-                                            active=True)
+                                              active=True)
                 request.session['voucher_id'] = voucher.id
                 messages.success(request, 'Coupon Accepted.')
             except ObjectDoesNotExist:
                 messages.success(request, 'Coupon Does not exist.')
-                request.session['voucher_id'] = None
                 code = None
         return redirect("view_cart")
