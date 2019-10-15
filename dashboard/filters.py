e57a63c36f998ca02a1a349f77db2a0f363b7a2b
gello94 <gello94@live.it>
2019-10-15 14:32:17 +0100

    filter for orders in dashboard added

---
+++ dashboard/filters.py
@@ -0,0 +1,8 @@
+import django_filters
+from orders.models import OrderLineItem, Order
+
+class OrdersFilter(django_filters.FilterSet):
+    
+    class Meta:
+        model = Order
+        fields = {'date', 'id'}
