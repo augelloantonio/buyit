5f714d7a99b3bb96a74c71dfce86f51319099280
gello94 <gello94@live.it>
2019-10-17 21:52:29 +0100

    added view of bestseller for most sold product

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from products.models import Product, Category
-from django.db.models import Avg, F
+from django.db.models import Avg, F, Count
 
 
 def index(request):
@@ -14,9 +14,9 @@ def index(request):
         product_category=category_id_latest).order_by('-published_date')[:9]
 
     # Made a list of product with category bestsellers
-    category_id_bestsellers = Category.objects.get(id=2)
-    products_bestsellers = Product.objects.filter(
-        product_category=category_id_bestsellers).order_by('-published_date')[:9]
+
+    products_bestsellers = products.order_by('-quantity_sold')
+
 
     return render(request, "index.html", {"products": products, "product_reviews": product_reviews,
                                           'products_latest': products_latest, 'products_bestsellers': products_bestsellers})
