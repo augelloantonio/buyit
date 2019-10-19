d48465c7fb626827a9c4bdc7952000b8f49070ce
gello94 <gello94@live.it>
2019-10-19 16:10:28 +0100

    simply way to display latest and bestsellers ordering them by added date and by n of products sold

--- home/views.py
+++ home/views.py
@@ -5,18 +5,15 @@ from django.db.models import Avg, F, Count
 
 def index(request):
     products = Product.objects.all()
-    product_reviews = Product.objects.annotate(avg_rating=Avg('review__rating'),
-                                               product_id=F("id"))
+    product_reviews = products.annotate(avg_rating=Avg('review__rating'),
+                                        product_id=F("id"))
 
     # Made a list of product with category latest
-    category_id_latest = Category.objects.get(id=1)
-    products_latest = Product.objects.filter(
-        product_category=category_id_latest).order_by('-published_date')[:9]
+    products_latest = products.order_by('-published_date')[:9]
 
     # Made a list of product with category bestsellers
 
     products_bestsellers = products.order_by('-quantity_sold')
-
 
     return render(request, "index.html", {"products": products, "product_reviews": product_reviews,
                                           'products_latest': products_latest, 'products_bestsellers': products_bestsellers})
