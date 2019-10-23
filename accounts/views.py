35b94588e93c0e9496d2eb5ca685b7827ab0063e
gello94 <gello94@live.it>
2019-10-23 17:17:30 +0100

    trying to fix the user_id session error with no result

--- accounts/views.py
+++ accounts/views.py
@@ -42,34 +42,37 @@ def logout(request):
     return redirect(reverse('index'))
 
 
-def login(request):
-    """A view that manages the login form"""
+def personal_login(request):
     if request.user.is_authenticated:
         return redirect(reverse('profile'))
     else:
-        if request.method == 'POST':
-            user_form = UserLoginForm(request.POST)
-            if user_form.is_valid():
-                user = auth.authenticate(request.POST['username_or_email'],
-                                         password=request.POST['password'])
+        return login(request)
 
-                if user:
-                    auth.login(request, user)
-                    messages.error(request, "You have successfully logged in")
 
-                    if request.GET and request.GET['next'] != '':
-                        next = request.GET['next']
-                        return HttpResponseRedirect(next)
-                    else:
-                        return redirect(reverse('index'))
+def login(request):
+    if request.method == 'POST':
+        user_form = UserLoginForm(request.POST)
+        if user_form.is_valid():
+            user = auth.authenticate(request.POST['username_or_email'],
+                                     password=request.POST['password'])
+
+            if user:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+
+                if request.GET and request.GET['next'] != '':
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
                 else:
-                    user_form.add_error(
-                        None, "Your username or password are incorrect")
-        else:
-            user_form = UserLoginForm()
+                    return redirect(reverse('index'))
+            else:
+                user_form.add_error(
+                    None, "Your username or password are incorrect")
+    else:
+        user_form = UserLoginForm()
 
-        args = {'user_form': user_form, 'next': request.GET.get('next', '')}
-        return render(request, 'login.html', args)
+    args = {'user_form': user_form, 'next': request.GET.get('next', '')}
+    return render(request, 'login.html', args)
 
 
 def register(request):
