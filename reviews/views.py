a4712e3106577393326c6a8c7032e4ec8f6862c8
gello94 <gello94@live.it>
2019-10-24 12:47:46 +0100

    user variable changed in costumer

--- reviews/views.py
+++ reviews/views.py
@@ -13,25 +13,25 @@ def review_detail(request, review_id):
     return render(request, 'review.html', {'review': review})
 
 
-@login_required()
 def add_review(request, product_id):
-    user = request.user
-    product = get_object_or_404(Product, pk=product_id)
-    form = ReviewForm(request.POST)
 
-    if Review.objects.filter(product_id=product, user=user):
-        messages.warning(request, "We are sorry, but you can't review twice!")
-        return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
-    else:
-        if form.is_valid():
-            rating = form.cleaned_data['rating']
-            comment = form.cleaned_data['comment']
-            review = Review()
-            review.product_id = product.id
-            review.rating = rating
-            review.comment = comment
-            review.pub_date = datetime.datetime.now()
-            review.save()
+        customer = request.user
+        product = get_object_or_404(Product, pk=product_id)
+        form = ReviewForm(request.POST)
+
+        if Review.objects.filter(product_id=product, user=customer):
+            messages.warning(request, "We are sorry, but you can't review twice!")
             return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
+        else:
+            if form.is_valid():
+                rating = form.cleaned_data['rating']
+                comment = form.cleaned_data['comment']
+                review = Review()
+                review.product_id = product.id
+                review.rating = rating
+                review.comment = comment
+                review.pub_date = datetime.datetime.now()
+                review.save()
+                return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
 
         return render(request, "addreview.html", {'form': form, 'product': product})
