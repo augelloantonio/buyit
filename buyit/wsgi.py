44d4d0927e5dd251ada6b1284b3775fdfb37e6d5
gello94 <gello94@live.it>
2019-10-24 16:51:47 +0100

    added .dev after settings

--- buyit/wsgi.py
+++ buyit/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buyit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buyit.settings.dev')
 
 application = get_wsgi_application()
