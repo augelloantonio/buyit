226916cf6e18879b96d1d84d61dcd98544afee70
gello94 <gello94@live.it>
2019-10-24 19:16:39 +0100

    removed unused field

--- voucher/models.py
+++ voucher/models.py
@@ -4,7 +4,6 @@ from datetime import datetime, date
 
 class Voucher(models.Model):
     code = models.CharField(max_length=15)
-    amount = models.FloatField(default=0)
     price_reducing = models.IntegerField(default=0)
     active = models.BooleanField(default=False)
 
