ca44f05c3d68537d3edcb207a7e408949d6066a9
gello94 <gello94@live.it>
2019-10-24 12:44:42 +0100

    removed all the calls to the voucher, moved in voucher contexts.py

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,8 +1,5 @@
 from django.shortcuts import get_object_or_404
 from products.models import Product
-from voucher.models import Voucher
-from voucher.forms import VoucherForm
-from decimal import Decimal
 
 
 def cart_contents(request):
@@ -12,37 +9,15 @@ def cart_contents(request):
     """
     cart = request.session.get('cart', {})
 
-    # Handle bug if voucher_id is not in session
-    code = None
-
-    if 'voucher_id':
-        voucher_id = request.session.get('voucher_id')
-        try:
-            code = Voucher.objects.get(id=voucher_id)
-        except Voucher.DoesNotExist:
-            code = None
-    else:
-        None
-
     cart_items = []
     total = 0
     product_count = 0
-    new_total = 0
 
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
-        # Handle bug if voucher_id is not in session
-        if code != None:
-            discount = (code.price_reducing/Decimal('100'))*total
-            new_total = total - discount
-        else:
-            new_total = total
         product_count += quantity
         cart_items.append({'id': id, 'quantity': quantity,
                            'product': product})
-
-    voucher_form = VoucherForm(request.POST)
     
-    return {'cart_items': cart_items, 'total': total, 'product_count': product_count, 'voucher_form': voucher_form,
-            'new_total': new_total, 'code': code}
+    return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
