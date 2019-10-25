d89c2b650f52de8657be5ddbf91d938a34c00e4a
gello94 <gello94@live.it>
2019-10-25 21:56:29 +0100

    navbar effect and bug of auto scroll on iphone fixed

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,49 +7,49 @@
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
 };
 
 // jQuery functions
 $(document).ready(function() {
-  //collapse sidebar in dashboard
+    //collapse sidebar in dashboard
 
-  $(".btn-expand-collapse").click(function(e) {
-    $("#sidebar-wrapper").toggleClass("collapsed");
-  });
+    $(".btn-expand-collapse").click(function(e) {
+        $("#sidebar-wrapper").toggleClass("collapsed");
+    });
 
-  // Add style to total price if discount code is applied
-  if ($("#new_total").is(":visible")) {
-    $("#total").addClass("basic_price_lined");
-    console.log("visible");
-  } else {
-    $("#total").removeClass("basic_price_lined");
-    console.log("not visible");
-  }
+    // Add style to total price if discount code is applied
+    if ($("#new_total").is(":visible")) {
+        $("#total").addClass("basic_price_lined");
+        console.log("visible");
+    } else {
+        $("#total").removeClass("basic_price_lined");
+        console.log("not visible");
+    }
 
-  // Auto Hide Messages
-  setTimeout(function() {
-    $(".alert").hide();
-  }, 2000);
+    // Auto Hide Messages
+    setTimeout(function() {
+        $(".alert").hide();
+    }, 2000);
 });
 
 // Add script for spinner
 $("#demo9").TouchSpin({
-  buttondown_class: "btn btn-link",
-  buttonup_class: "btn btn-link"
+    buttondown_class: "btn btn-link",
+    buttonup_class: "btn btn-link"
 });
 
 // Search Form activator
 function searchToggle() {
-  if ($("#input-search").hasClass("hidden-input")) {
-    $("#input-search").removeClass("hidden-input");
-  } else {
-    $("#input-search").addClass("hidden-input");
-  }
+    if ($("#input-search").hasClass("hidden-input")) {
+        $("#input-search").removeClass("hidden-input");
+    } else {
+        $("#input-search").addClass("hidden-input");
+    }
 }
