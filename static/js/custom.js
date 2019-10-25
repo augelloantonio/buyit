08eeae130e776d95c95a0a13ef3f1eb881fb6fb4
gello94 <gello94@live.it>
2019-10-25 22:49:35 +0100

    carousel now displaying only one card on mobile and 2 card on medium screen, ux improved and less caos in the home page

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,13 +7,15 @@
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "";
-    } else {
-        document.getElementById("navbar").style.top = "-60px";
+    if (window.pageYOffset >= 60) {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "";
+        } else {
+            document.getElementById("navbar").style.top = "-60px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
 };
 
 // jQuery functions
