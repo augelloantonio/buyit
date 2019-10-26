38d6d6260f28feed3d979e263e2219ed10e63536
gello94 <gello94@live.it>
2019-10-26 20:43:41 +0100

    removed unused link and def from view

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import add_review, review_detail
+from .views import add_review
 
 urlpatterns = [
-    path('', views.review_detail, name="reviews"),
-    path('review/<int:review_id>', views.review_detail, name='review_detail'),
-    path('addreview/product/<int:product_id>', views.add_review, name='add_review'),
+    path('addreview/product/<int:product_id>',
+         views.add_review, name='add_review'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- reviews/views.py
+++ reviews/views.py
@@ -8,11 +8,6 @@ from products.models import Product
 from .models import Review
 
 
-def review_detail(request, review_id):
-    review = get_object_or_404(Review, pk=review_id)
-    return render(request, 'review.html', {'review': review})
-
-
 @login_required
 def add_review(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
