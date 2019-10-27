5ad0d490c92665d0e41c03ec21b60c847422a242
gello94 <gello94@live.it>
2019-10-27 12:00:53 +0000

    unuser if user is anonymous removed, the decoretor already check if user is logged of not

--- reviews/views.py
+++ reviews/views.py
@@ -14,27 +14,23 @@ def add_review(request, product_id):
     form = ReviewForm(request.POST)
     customer = request.user
 
-    if request.user.is_anonymous:
-        messages.warning(request, "We are sorry, but you can't review twice!")
+    customer = request.user
+    if Review.objects.filter(product_id=product, user=customer):
+        messages.warning(
+            request, "We are sorry, but you can't review twice!")
         return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
     else:
-        customer = request.user
-        if Review.objects.filter(product_id=product, user=customer):
-            messages.warning(
-                request, "We are sorry, but you can't review twice!")
+        if form.is_valid():
+            rating = form.cleaned_data['rating']
+            comment = form.cleaned_data['comment']
+            review_summary = form.cleaned_data['review_summary']
+            review = Review()
+            review.review_summary = review_summary
+            review.product_id = product.id
+            review.rating = rating
+            review.comment = comment
+            review.pub_date = datetime.datetime.now()
+            review.save()
             return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
-        else:
-            if form.is_valid():
-                rating = form.cleaned_data['rating']
-                comment = form.cleaned_data['comment']
-                review_summary = form.cleaned_data['review_summary']
-                review = Review()
-                review.review_summary = review_summary
-                review.product_id = product.id
-                review.rating = rating
-                review.comment = comment
-                review.pub_date = datetime.datetime.now()
-                review.save()
-                return HttpResponseRedirect(reverse('product_detail', args=(product.id,)))
 
     return render(request, "addreview.html", {'form': form, 'product': product})
