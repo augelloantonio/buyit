f0154402dfa730e38b176c1fa3b31d175f5f813a
gello94 <gello94@live.it>
2019-11-06 14:35:49 +0000

    alert messagge added when user sent an email by contact form

--- static/js/custom.js
+++ static/js/custom.js
@@ -68,9 +68,13 @@ function sendMail(contactForm) {
         .then(
             function (response) {
                 console.log("SUCCESS", response);
+                var alert = '<div class="alert alert-success" role="alert"> Your message has been sent.You will receive an answer as soon as possible.</div>'
+                document.getElementById("email-alert").innerHTML = alert;
             },
             function (error) {
                 console.log("FAILED", error);
+                var alert = '<div class="alert alert-danger" role="alert">Your message was not sent. Please check that all the details are correct.</div>'
+                document.getElementById("email-alert").innerHTML = alert;
             }
         );
     return false; // To block from loading a new page
