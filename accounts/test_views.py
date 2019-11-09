4d0648ca5f351530aa48530483d0a14360d1b6b6
gello94 <gello94@live.it>
2019-11-09 20:23:07 +0000

    tested registration form with missing form value and tested login with missing form input

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -39,6 +39,41 @@ class TestAccountViewsRegister(TestCase):
         self.assertEqual(str(messages[0]),
                          'You have successfully registered, an email has been sent to you.')
 
+    def test_registering_a_new_user_with_incorrect_form_input(self):
+        ''' test registering a user with incorrect input '''
+
+        # post an invalid form with no password2 inputted
+        page = self.client.post('/accounts/register/',
+                                {'email': 'test@email.com',
+                                 'username': 'test',
+                                 'password1': 'password',
+                                 'password2': 'password2'},
+                                follow=True)
+        # check for status code 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is register.html
+        self.assertTemplateUsed(page, "register.html")
+
+    def test_registering_a_new_user_with_valid_form_input(self):
+        ''' test registering a user with valid input '''
+
+        # post input to the register url
+        page = self.client.post('/accounts/register/',
+                                {'email': 'test@email.com',
+                                 'username': 'test',
+                                 'password1': '!123Password',
+                                 'password2': '!123Password'},
+                                follow=True)
+        # check for a status 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is profile.html page
+        self.assertTemplateUsed(page, "profile.html")
+        # check theat the stored message is
+        # equal to the expected message
+        messages = list(get_messages(page.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'You have successfully registered, an email has been sent to you.')
+
     def test_registering_a_new_user_with_invalid_form_input(self):
         ''' test registering a user with invalid input '''
 
@@ -124,12 +159,29 @@ class TestAccountViewsLogin(TestCase):
                          'You have successfully logged in')
 
     def test_login_user_with_invalid_form_input(self):
+        ''' test login with invalid form '''
+
+        # create a user
+        user = User.objects.create_user('user',
+                                        'myemail@test.com',
+                                        '!123Password')
+        # login with the wrong password
+        page = self.client.post('/accounts/login/',
+                                {'username_or_email': '',
+                                 'password': 'xxx'},
+                                follow=True)
+        # check for a status code 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is login.html page
+        self.assertTemplateUsed(page, "login.html")
+
+    def test_login_user_with_wrong_form_input(self):
         ''' test login with invalid information '''
 
         # create a user
         user = User.objects.create_user('user',
                                         'myemail@test.com',
-                                        'password')
+                                        '!123Password')
         # login with the wrong password
         page = self.client.post('/accounts/login/',
                                 {'username_or_email': 'user',
@@ -176,8 +228,8 @@ class TestAccountViewsLogout(TestCase):
         self.assertTemplateUsed(page, "index.html")
 
 
-class TestAccountViewsProfile(TestCase):
-    ''' test the profile views '''
+class TestEditAccountViewsProfile(TestCase):
+    ''' test the edit profile views '''
 
     def test_get_profile_page_when_someone_logged_in(self):
         ''' test profile view when someone is logged in '''
@@ -205,3 +257,187 @@ class TestAccountViewsProfile(TestCase):
         self.assertEqual(page.status_code, 200)
         # check Template Used is login.html page
         self.assertTemplateUsed(page, "login.html")
+
+    def test_edit_profile_with_valid_ammendments(self):
+        ''' test editing the profile with valid ammendments '''
+
+        # create a user
+        user = User.objects.create_user('username',
+                                        'test@test.com',
+                                        'password')
+        # login user
+        self.client.login(username='username',
+                          password='password')
+        # edit profile url
+        page = self.client.post("/accounts/editprofile/",
+                                {'username': 'new',
+                                 'email': 'newemail@test.com',
+                                 'user': 'user'},
+                                follow=True)
+        # check the status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is profile.html page
+        self.assertTemplateUsed(page, "profile.html")
+
+    def test_edit_profile_with_invalid_form(self):
+        ''' test editing the profile with invalid form '''
+
+        # create a user
+        user = User.objects.create_user('username',
+                                        'test@test.com',
+                                        'password')
+        # login user
+        self.client.login(username='username',
+                          password='password')
+        # edit profile url
+        page = self.client.post("/accounts/editprofile/",
+                                {'username': '',
+                                 'email': 'newemail@test.com',
+                                 'user': ''},
+                                follow=True)
+        # check the status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is profile.html page
+        self.assertTemplateUsed(page, "editprofile.html")
+
+    def test_edit_profile_to_email_address_already_in_use(self):
+        ''' test editing the profile with an
+            email address already in use by
+            another user '''
+
+        # create a user
+        user = User.objects.create_user('username',
+                                        'test@test.com',
+                                        'password')
+        # create a second user
+        user2 = User.objects.create_user('new',
+                                         'newemail@test.com',
+                                         'password')
+        # login the first user
+        self.client.login(username='username',
+                          password='password')
+        # post edit profile form details to the edit profile url
+        page = self.client.post("/accounts/editprofile/",
+                                {'username': 'username',
+                                 'email': 'newemail@test.com',
+                                 'user': 'user'},
+                                follow=True)
+        # check Template Used is editprofile.html page
+        self.assertTemplateUsed(page, "editprofile.html")
+        # check the status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check the message stored
+        # is equal to the expected message
+        messages = list(get_messages(page.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Somebody with this email '
+                         'address is already registered '
+                         'please enter a unique email address')
+
+    def test_edit_profile_when_username_is_in_use(self):
+        ''' test editing the profile to a username
+            that is already in use '''
+
+        # create a user
+        user = User.objects.create_user('username',
+                                        'myemail@test.com',
+                                        'password')
+        # create a second user
+        user2 = User.objects.create_user('change',
+                                         'change@change.com',
+                                         'password')
+        # login user
+        self.client.login(username='username',
+                          password='password')
+        # try to edit user1 username to same as user2
+        page = self.client.post("/accounts/editprofile/",
+                                {'username': 'change',
+                                 'email': 'myemail@test.com',
+                                 'user': 'user'},
+                                follow=True)
+        # check Template Used is editprofile.html page
+        self.assertTemplateUsed(page, "editprofile.html")
+        # check the status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check the message stored is equal
+        # to the expected message
+        messages = list(get_messages(page.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Somebody already has this '
+                         'username please enter a '
+                         'unique username')
+
+
+class TestEditUserPasswordViews(TestCase):
+    ''' test the edit password views '''
+
+    def test_get_change_password_page_when_someone_logged_in(self):
+        ''' test profile view when someone is logged in '''
+
+        # create a user
+        user = User.objects.create_user('username',
+                                        'myemail@test.com',
+                                        'password')
+        # login the user
+        self.client.login(username='username',
+                          password='password')
+        # profile url
+        page = self.client.get("/accounts/password/", follow=True)
+        # check status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is profile.html page
+        self.assertTemplateUsed(page, "editpassword.html")
+
+    def test_change_password_page_when_no_one_logged_in(self):
+        ''' test change password view when no-one is logged in '''
+
+        # profile url
+        page = self.client.get("/accounts/password/", follow=True)
+        # check status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is login.html page
+        self.assertTemplateUsed(page, "login.html")
+
+    def test_post_change_password_with_valid_form(self):
+        ''' test edit password post with valid form '''
+
+        # create a user
+        user = User.objects.create_user('username',
+                                        'myemail@test.com',
+                                        '!123Password')
+        # login the user
+        self.client.login(username='username',
+                          password='!123Password')
+
+        # profile url
+        page = self.client.post("/accounts/password/",
+                                {'old_password': '!123Password',
+                                 'new_password1': '!123NewPassword',
+                                 'new_password2': '!123NewPassword'},
+                                follow=True)
+        # check status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is profile.html page
+        self.assertTemplateUsed(page, "profile.html")
+
+    def test_post_change_password_with_invalid_form(self):
+        ''' test edit password post with invalid form '''
+
+        # create a user
+        user = User.objects.create_user('username',
+                                        'myemail@test.com',
+                                        '!123Password')
+        # login the user
+        self.client.login(username='username',
+                          password='!123Password')
+
+        # profile url
+        page = self.client.post("/accounts/password/",
+                                {'old_password': '!123Password',
+                                 'new_password1': '!123NewPassword',
+                                 'new_password2': '123Password'},
+                                follow=True)
+        # check status code is 200
+        self.assertEqual(page.status_code, 200)
+        # check Template Used is profile.html page
+        self.assertTemplateUsed(page, "editpassword.html")
